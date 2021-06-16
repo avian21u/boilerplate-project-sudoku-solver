1d20f937bc1f4d4fa0cc183de6118e137c1ad615
Glitch (fcc-sudoku-solver-) <none>
2021-06-16 05:52:27 +0000

    👼🍿 Updated with Glitch - Passed all FCC Tests

--- routes/api.js
+++ routes/api.js
@@ -4,15 +4,121 @@ const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
   
-  let solver = new SudokuSolver();
+let solver = new SudokuSolver();
 
+  /************************************************************/
+  /*
+  You can POST to /api/check an object containing puzzle, coordinate, 
+  and value where the coordinate is the letter A-I indicating the row, 
+  followed by a number 1-9 indicating the column, and value is a number from 1-9.
+  */
+  
   app.route('/api/check')
     .post((req, res) => {
-
-    });
+      // Validate puzzle
+      var puzzle = req.body.puzzle;
+      if (!solver.validate(puzzle)){
+        if (puzzle.length !== 81) {
+          res.json({ error: 'Expected puzzle to be 81 characters long' })
+        }
+        else {
+          var regex = /^(\d|[.])*$/;
+          if (!regex.test(puzzle)) {
+            return res.json({ error: 'Invalid characters in puzzle' })  
+          }
+          else {
+            return res.json({ error: 'Puzzle cannot be solved' })    
+          }
+        }
+      };
     
+      var coordinate = req.body.coordinate;
+      var value = req.body.value;
+      // Validate inputs
+      // Fields are empty
+      if (!puzzle || !coordinate || !value || puzzle.trim() === "" || coordinate.trim() === "" || value.trim() === "") {
+        return res.json({ error: "Required field(s) missing" });
+      }
+      // Fields are not empty
+      // If the value submitted to /api/check is not a number between 1 and 9, the returned values will be { error: 'Invalid value' }
+      if (Number.isNaN(parseInt(value)) || value < 1 || value > 9) {
+        return res.json({error: 'Invalid value'})
+      }
+      else if (coordinate.length == 2) {
+        var row = coordinate[0];
+        var col = Number.parseInt(coordinate[1]); //1-based index
+        var rows = "ABCDEFGHI";
+        var rowIndex = rows.indexOf(row);
+        if (rowIndex !== -1 && col >=1 && col <= 9) {
+            // If value submitted to /api/check is already placed in puzzle on that coordinate, the returned value will be an object containing a valid property with true if value is not conflicting.
+            var index = (9 * rowIndex) + (col - 1);
+            if (puzzle[index] === value) {
+              return res.json({valid: true});
+            }
+            // The return value from the POST to /api/check will be an object containing a valid property, 
+            // which is true if the number may be placed at the provided coordinate and false if the number may not. 
+            else {
+              // If false, the returned object will also contain a conflict property which is an array containing 
+              // the strings "row", "column", and/or "region" depending on which makes the placement invalid.
+              var output = {};
+              var conflict = [];
+              
+              if (!solver.checkRowPlacement(puzzle, row, col, value)) {
+                conflict.push('row')
+              };
+              if (!solver.checkColPlacement(puzzle, row, col, value)) {
+                conflict.push('column')
+              };
+              if (!solver.checkRegionPlacement(puzzle, row, col, value)) {
+                conflict.push('region')
+              };
+              
+              if (conflict.length > 0) {
+                output['valid'] = false;
+                output['conflict'] = conflict;
+              }
+              else {
+                output['valid'] = true;
+              }
+              return res.json(output);
+            }
+        } // If the coordinate submitted to api/check does not point to an existing grid cell, the returned value will be { error: 'Invalid coordinate'}
+        else return res.json({error: 'Invalid coordinate'});       
+      } else return res.json({error: 'Invalid coordinate'});
+    });
+  
+  
+  
+  /************************************************************/
+  // You can POST /api/solve with form data containing puzzle which will be a string containing a combination of numbers (1-9) and periods . to represent empty spaces. 
+  
   app.route('/api/solve')
     .post((req, res) => {
-
+      // Validate puzzle
+      var puzzle = req.body.puzzle;
+      //2. If the object submitted to /api/solve is missing puzzle, the returned value will be { error: 'Required field missing' } 
+      if(!puzzle || puzzle.trim() === "") {
+        return res.json({ error: 'Required field missing' }) 
+      }
+      // 3. If the puzzle submitted to /api/solve contains values which are not numbers or periods, the returned value will be { error: 'Invalid characters in puzzle' }
+      // 4. If the puzzle submitted to /api/solve is greater or less than 81 characters, the returned value will be { error: 'Expected puzzle to be 81 characters long' }
+    //5. If the puzzle submitted to /api/solve is invalid or cannot be solved, the returned value will be { error: 'Puzzle cannot be solved' }
+      else if (!solver.validate(puzzle)){
+          if (puzzle.length !== 81) {
+          res.json({ error: 'Expected puzzle to be 81 characters long' })
+        }
+        else {
+          var regex = /^(\d|[.])*$/;
+          if (!regex.test(puzzle)) {
+            return res.json({ error: 'Invalid characters in puzzle' })  
+          }
+          else {
+            return res.json({ error: 'Puzzle cannot be solved' })    
+          }
+        }
+      } else {
+          var solution = solver.solve(puzzle);
+          return res.json({ solution: solution });
+      }
     });
 };
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,81 @@
 const chai = require('chai');
 const assert = chai.assert;
 
+const Solutions = require('../controllers/puzzle-strings.js');
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+let solver = new Solver();
 
 suite('UnitTests', () => {
-
+  var valid_input = Solutions.puzzlesAndSolutions[0][0];
+  var invalid_input_1 = '1.5..2..4..63.12.!.!..5.....9..1..ab8.2.3674.3.7.2..9.47...8..1..16...!926914.37.';
+  var invalid_input_2 = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914'
+  
+// Logic handles a valid puzzle string of 81 characters
+  test('Logic handles a valid puzzle string of 81 characters', function(done){
+    Solutions.puzzlesAndSolutions.forEach((solution) => {
+      assert.equal(solver.validate(solution[0]), true, "String can only be 81 characters."); 
+    });
+    done();
+  });
+// Logic handles a puzzle string with invalid characters (not 1-9 or .)
+  test('Logic handles a puzzle string with invalid characters (not 1-9 or .)', function(done){
+    assert.equal(solver.validate(invalid_input_1), false, "String cannot include alphabets and special characters.");
+    done();
+  });
+// Logic handles a puzzle string that is not 81 characters in length
+  test('Logic handles a puzzle string that is not 81 characters in length', function(done){
+    assert.equal(solver.validate(invalid_input_2), false, "String can only be 81 characters.");
+    done();
+  });
+  
+// Logic handles a valid row placement
+  test('Logic handles a valid row placement', function(done){
+    assert.equal(solver.checkRowPlacement(valid_input , "B", 2, 4), true, "Logic handles a valid row placement.");
+    done();
+  });
+// Logic handles an invalid row placement
+  test('Logic handles an invalid row placement', function(done){
+    assert.equal(solver.checkRowPlacement(valid_input, "G", 4, 8), false, "Logic handles an invalid row placement - value already in row.");
+    done();
+  });
+// Logic handles a valid column placement
+  test('Logic handles a valid column placement', function(done){
+    assert.equal(solver.checkColPlacement(valid_input, "G", 4, 2), true, "Logic handles a valid column placement.");
+    done();
+  });
+// Logic handles an invalid column placement
+  test('Logic handles a valid column placement', function(done){
+    assert.equal(solver.checkColPlacement(valid_input, "D", 8, 4), false, "Logic handles an invalid column placement - existing value in col.");
+    done();
+  });
+// Logic handles a valid region (3x3 grid) placement
+  test('Logic handles a valid region (3x3 grid) placement', function(done){
+    assert.equal(solver.checkRegionPlacement(valid_input, "G", 3, 3), true, "Logic handles a valid region (3x3 grid) placement.");
+    done();
+  });
+// Logic handles an invalid region (3x3 grid) placement
+  test('Logic handles an invalid region (3x3 grid) placement', function(done){
+    assert.equal(solver.checkRegionPlacement(valid_input, "E", 4, 2), false, "Logic handles an invalid region (3x3 grid) placement - existing value in region.");
+    done();
+  });
+// Valid puzzle strings pass the solver
+  test('Valid puzzle strings pass the solver', function(done){
+    Solutions.puzzlesAndSolutions.forEach((solution) => {
+        assert.equal(solver.solve(solution[0]), solution[1], "Valid puzzle strings pass the solver.");  
+    });
+    done();
+  });
+// Invalid puzzle strings fail the solver
+  test('Invalid puzzle strings fails the solver', function(done){
+    assert.equal(solver.solve(invalid_input_1), false, "Invalid puzzle strings with special characters fails the solver.");
+    assert.equal(solver.solve(invalid_input_2), false, "Invalid puzzle strings less than 81 chars fails the solver.");
+    done();
+  });
+// Solver returns the the expected solution for an incomplete puzzle
+  test('Solver returns the the expected solution for an incomplete puzzle', function(done){
+    Solutions.puzzlesAndSolutions.forEach((solution) => {
+        assert.equal(solver.solve(solution[0]), solution[1], "Solver returns the the expected solution for an incomplete puzzle");  
+    });
+    done();
+  });
 });
